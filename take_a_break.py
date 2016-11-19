7bfc0d1e9eae2088b5225a6b666ddb6a8ffbf987
Saim <saim.khan@colorado.edu>
2016-11-19 11:26:17 -0700

    modified variable

--- take_a_break.py
+++ take_a_break.py
@@ -2,7 +2,7 @@ import webbrowser
 import time
 
 i=0
-while(i<=2):
+while(i<3):
 	time.sleep(5)				# time should ideally be approx 3 hours
 	print("Please take some timeout to see your favorite video")
 	time.sleep(2)
